8843bca00fe80042e7ecb4e38483d4d2a6ebfcd5
Oh-Myeongjae <omjcws@gmail.com>
2023-08-15 20:43:17 +0900

    feat: 폴더구조 초기세팅

---
+++ src/main/java/com/teamsupercat/roupangbackend/RoupangbackendApplication.java
@@ -0,0 +1,13 @@
+package com.teamsupercat.roupangbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoupangbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoupangbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamsupercat/roupangbackend/RoupangbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsupercat.roupangbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoupangbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
