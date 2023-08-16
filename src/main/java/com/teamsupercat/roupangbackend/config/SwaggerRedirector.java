c62fbe5a5a6d6b8895142ff7bbd73623a501ad08
김주환 <kjh71047@naver.com>
2023-08-16 11:55:35 +0900

    Swagger 접속경로 추가(경로라우팅) 주소: http://localhost:8080/TeamSuperCat

---
+++ src/main/java/com/teamsupercat/roupangbackend/config/SwaggerRedirector.java
@@ -0,0 +1,15 @@
+package com.teamsupercat.roupangbackend.config;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/TeamSuperCat")
+public class SwaggerRedirector {
+    @GetMapping
+    public String api() {
+        return "redirect:/swagger-ui.html";
+    }
+}
