b1ec797a8c75207d670c30e61e626d394d8eea7f
김주환 <kjh71047@naver.com>
2023-08-17 06:07:00 +0900

    feat: (Custom에러코드 추가 설정)

--- src/main/java/com/teamsupercat/roupangbackend/common/ErrorCode.java
+++ src/main/java/com/teamsupercat/roupangbackend/common/ErrorCode.java
@@ -7,7 +7,20 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    POST_NOTFOUND(HttpStatus.BAD_REQUEST,"찾을수 없는 게시글입니다.");
+    //
+    NOTFOUND_USER(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
+
+    // 게시글
+    POST_NOTFOUND(HttpStatus.BAD_REQUEST, "찾을수 없는 게시글입니다."),
+
+    // 상품
+    NOTFOUND_PRODUCT(HttpStatus.NOT_FOUND, "해당 상품을 찾을 수 없습니다."),
+
+    NOTFOUND_PRODUCT_STOCK(HttpStatus.NOT_FOUND, "해당 상품의 재고가 남아있지 않습니다."),
+
+    OUT_OF_STOCK(HttpStatus.NOT_FOUND, "개의 상품 재고수량보다 많이 선택하셨습니다."),
+    ;
+
 
     private final HttpStatus httpStatus;
     private final String errorMsg;
