aac4d6fbbdfaefa89e426955eba7b4911f6af49f
Oh-Myeongjae <omjcws@gmail.com>
2023-08-18 18:20:14 +0900

    feat(시큐리티): UserDetails객체에 멤버idx항목 추가

--- src/main/java/com/teamsupercat/roupangbackend/controller/MemberController.java
+++ src/main/java/com/teamsupercat/roupangbackend/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.teamsupercat.roupangbackend.controller;
 
 import com.teamsupercat.roupangbackend.common.ResponseDto;
+import com.teamsupercat.roupangbackend.dto.CustomUserDetail.CustomUserDetail;
 import com.teamsupercat.roupangbackend.dto.member.DuplicateCheckDto;
 import com.teamsupercat.roupangbackend.dto.member.LoginRequestDto;
 import com.teamsupercat.roupangbackend.dto.member.SignupRequestDto;
@@ -8,7 +9,6 @@ import com.teamsupercat.roupangbackend.service.MemberService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -28,7 +28,7 @@ public class MemberController {
     }
     @ApiOperation(value = "회원탈퇴")
     @PatchMapping(value = "/delete")
-    public ResponseDto<?> signOut(@AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseDto<?> signOut(@AuthenticationPrincipal CustomUserDetail userDetails) {
         String memberEmail = userDetails.getUsername();
         return memberService.deleteMember(memberEmail);
     }
@@ -40,7 +40,7 @@ public class MemberController {
     }
     @ApiOperation(value = "로그아웃")
     @PatchMapping(value = "/logout")
-    public ResponseDto<?> logout(@AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseDto<?> logout(@AuthenticationPrincipal CustomUserDetail userDetails) {
         String memberEmail = userDetails.getUsername();
         return memberService.logoutMember(memberEmail);
     }
