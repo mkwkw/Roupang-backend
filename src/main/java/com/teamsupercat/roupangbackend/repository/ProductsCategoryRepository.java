e1d79ff77e73dae7cc6430df787b4772b8687c23
Kyoungwon Min <mkkw2047@gmail.com>
2023-08-18 15:54:04 +0900

    feat: 모든 카테고리 정보 조회 API 생성

--- src/main/java/com/teamsupercat/roupangbackend/repository/ProductsCategoryRepository.java
+++ src/main/java/com/teamsupercat/roupangbackend/repository/ProductsCategoryRepository.java
@@ -2,6 +2,8 @@ package com.teamsupercat.roupangbackend.repository;
 
 import com.teamsupercat.roupangbackend.entity.ProductsCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductsCategoryRepository extends JpaRepository<ProductsCategory,Integer> {
 }
