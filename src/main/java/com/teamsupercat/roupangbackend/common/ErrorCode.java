ef92c6827ee563aa01380415e62d9a88e5131708
dabin <113006964+dabinHam@users.noreply.github.com>
2023-08-20 01:21:56 +0900

    refactor : ErrorCode 병합 후 기존 파일 삭제
    - 기존 별도로 생성했던 ErrorCode 공통 코드에 병합 처리 후 별도로 생성했던 코드삭제

--- src/main/java/com/teamsupercat/roupangbackend/common/ErrorCode.java
+++ src/main/java/com/teamsupercat/roupangbackend/common/ErrorCode.java
@@ -27,6 +27,13 @@ public enum ErrorCode {
     LOGOUT_NOT_FOUND_EMAIL(HttpStatus.BAD_REQUEST, "등록되어 있지 않은 이메일 입니다."),
     LOGOUT_NOT_FOUND_TOKEN(HttpStatus.BAD_REQUEST, "맴버에 해당하는 토큰을 찾을수 없습니다."),
 
+    // 마이페이지
+    MY_PAGE_RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND,"요청한 정보를 찾을 수 없습니다."),
+    MY_PAGE_PERMISSION_DENIED(HttpStatus.FORBIDDEN,"권한이 거부되었습니다."),
+    MY_PAGE_EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT,"이미 등록된 이메일 주소입니다."),
+    MY_PAGE_NOT_AUTHORIZED(HttpStatus.UNAUTHORIZED,"권한이 없습니다. 로그인 후 이용해주세요."),
+    MY_PAGE_REQUIRED_INFO_MISSING(HttpStatus.BAD_REQUEST, "필수정보가 누락되었습니다."),
+
     // REQUEST
     NULL_FIELDS_REQUEST(HttpStatus.NOT_FOUND, "요청 중 일부 필드가 누락되었습니다."),
 
