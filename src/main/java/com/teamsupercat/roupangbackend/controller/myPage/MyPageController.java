1ab147d52d991176058da06c7f19d339efd5290d
dabin <113006964+dabinHam@users.noreply.github.com>
2023-08-20 02:04:28 +0900

    reactor : 마이페이지 조회 jwt추가 , ERROR CODE병합
    - 사용하지 않는 코드삭제
           - MyPageDto(조회request) 삭제 및 response 수정
           - userPassword 보안상 삭제 처리.
    
    - jwt를 사용하여 email값을 별도로 입력하지 않아도 로그인한 상태의 사용자 세션 값을 받아온다.
    
    - Controller 예외 처리
    servletRequest.getHeader("authorization") : 현재 시큐리티 예외처리가 아직 되어있지 않아서 현재 넣어두었음. 추후 시큐리티 예외처리 완료되면 삭제예정

--- src/main/java/com/teamsupercat/roupangbackend/controller/myPage/MyPageController.java
+++ src/main/java/com/teamsupercat/roupangbackend/controller/myPage/MyPageController.java
@@ -1,35 +1,43 @@
 package com.teamsupercat.roupangbackend.controller.myPage;
 
+import com.teamsupercat.roupangbackend.common.CustomException;
+import com.teamsupercat.roupangbackend.common.ErrorCode;
 import com.teamsupercat.roupangbackend.common.ResponseDto;
-import com.teamsupercat.roupangbackend.entity.Member;
 import com.teamsupercat.roupangbackend.service.MyPageService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Api(tags = "마이페이지 API")
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("api/v1/mypage")
 public class MyPageController {
 
     private final MyPageService myPageService;
-
-//    @ApiOperation(value = "마이페이지 조회")
-//    @GetMapping(value = "/{email}")
-//    public Member GetMyPage(@PathVariable("email") String email) {
-//        return myPageService.getMyPage(email);
-//    }
-
     @ApiOperation(value = "마이페이지 조회")
-    @GetMapping(value = "/{email}")
-    public ResponseDto<?> GetMyPage(@PathVariable("email") String email) {
-        
-        return ResponseDto.success(myPageService.getMyPage(email));
+    @GetMapping(value = "/")
+    public ResponseDto<?> getMyPage(
+            @AuthenticationPrincipal UserDetails userDetails,
+            HttpServletRequest servletRequest) {
+
+        // servletRequest.getHeader("authorization") : 추후 수정예정
+        if (servletRequest.getHeader("authorization") == null || userDetails == null)
+            throw new CustomException(ErrorCode.MY_PAGE_NOT_AUTHORIZED);
+
+        log.info("userDetails1 : {}", userDetails);
+        String memberEmail = userDetails.getUsername();
+
+        return myPageService.getMyPageInfo(memberEmail);
     }
 
 }
--- src/main/java/com/teamsupercat/roupangbackend/service/MyPageService.java
+++ src/main/java/com/teamsupercat/roupangbackend/service/MyPageService.java
@@ -1,23 +1,32 @@
 package com.teamsupercat.roupangbackend.service;
 
-import com.teamsupercat.roupangbackend.common.Mypage.MyPageException;
-import com.teamsupercat.roupangbackend.common.Mypage.type.MyPageErrorCode;
+import com.teamsupercat.roupangbackend.common.CustomException;
+import com.teamsupercat.roupangbackend.common.ErrorCode;
+import com.teamsupercat.roupangbackend.common.ResponseDto;
+import com.teamsupercat.roupangbackend.dto.myPage.MyPageDto;
 import com.teamsupercat.roupangbackend.entity.Member;
 import com.teamsupercat.roupangbackend.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MyPageService {
 
     private final MemberRepository memberRepository;
 
     @Transactional
-    public Member getMyPage(String email) {
-        return memberRepository.findByEmail(email)
-                .orElseThrow(() -> new MyPageException(MyPageErrorCode.RESOURCE_NOT_FOUND));
+    public ResponseDto<?> getMyPageInfo(String memberEmail) {
+
+        Member member = memberRepository.findByEmail(memberEmail)
+                .orElseThrow(() -> new CustomException(ErrorCode.MY_PAGE_PERMISSION_DENIED));
+
+        MyPageDto.MyPageResponse myPageResponse = MyPageDto.MyPageResponse.toResponse(member);
+
+        return ResponseDto.success(myPageResponse);
     }
 
 }
