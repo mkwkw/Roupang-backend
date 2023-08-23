bf5ee19ea5d2c3f36db45050e53a2aa2f97106bb
Kyoungwon Min <mkkw2047@gmail.com>
2023-08-23 14:55:00 +0900

    refactor: 컨트롤러 인자 순서 변경
    컨트롤러 인자 순서 변경

--- src/main/java/com/teamsupercat/roupangbackend/controller/ProductController.java
+++ src/main/java/com/teamsupercat/roupangbackend/controller/ProductController.java
@@ -43,14 +43,14 @@ public class ProductController {
 
     @ApiOperation("물품 조회 - 정렬 기준 설정 가능")
     @GetMapping("/products")
-    public ResponseDto<?> findAllProducts(@RequestParam(value = "order", required = false) String order, Pageable pageable) throws ParseException {
+    public ResponseDto<?> findAllProducts(Pageable pageable, @RequestParam(value = "order", required = false) String order) throws ParseException {
         Page<ProductResponse> products = productService.findProductsPagination(order, pageable);
         return ResponseDto.success(products);
     }
 
     @ApiOperation("카테고리별 물품 조회")
     @GetMapping("/products/category/{category_idx}")
-    public ResponseDto<?> findProductsByCategory(@RequestParam(value = "order", required = false) String order, @PathVariable("category_idx") Integer categoryIdx, Pageable pageable){
+    public ResponseDto<?> findProductsByCategory(@PathVariable("category_idx") Integer categoryIdx, Pageable pageable, @RequestParam(value = "order", required = false) String order){
         Page<ProductResponse> products = productService.findProductsByCategoryIdxPagination(order, categoryIdx, pageable);
         return ResponseDto.success(products);
     }
